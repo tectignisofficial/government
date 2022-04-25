c330a7a34143b9c6e28f13585478d2ae4a0e2779
keshridivya <dkeshari094@gmail.com>
2022-04-25 15:37:04 +0530

    c

--- 1/views/report/R5.php
+++ 1/views/report/R5.php
@@ -430,7 +430,7 @@ if (mysqli_num_rows($complaints)>0){
                   }else{
                     $page=1;                                      //if not
                   }
-                  $per_page=10;
+                  $per_page=2;
                   $start=($page-1)*$per_page;                  //fromwhich row no data fetch like 1-5 then 5-10
     
                   $no_Of_row_sql = "select COUNT(*) from complaint_form";
