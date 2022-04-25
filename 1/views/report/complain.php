c9306a136e0ed7f2a1ec8ca8f3b48c8a9367c66e
keshridivya <dkeshari094@gmail.com>
2022-04-25 13:23:08 +0530

    c

--- 1/views/report/complain.php
+++ 1/views/report/complain.php
@@ -65,12 +65,7 @@ if (mysqli_num_rows($complaints)>0){
   <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <style>
-  .fa-folder-open:before {
-    content: "\f07c";
-}
-.fa-sticky-note:before {
-    content: "\f249";
-}
+  
 .add{
     display:none;
 }
@@ -413,8 +408,8 @@ if (mysqli_num_rows($complaints)>0){
                     <td  class="no"><a href="../editform1.php?action=edit&id=<?php echo $arr["id"]; ?>" title="" id="edit" class="btn btn-success btn-xs delete_purchase edit ">
 		                          <i class="fas fa-edit"></i>
 		                        </a>
-                            <a href="#" title="" id="open" class="btn btn-success btn-xs delete_purchase">
-		                          <i  onclick="myFunction(this)"id="icon" class="fas fa-folder-open"></i>
+                            <a href="#" title="" id="open" class="btn btn-success btn-xs ">
+		                          <i  onclick="myFunction(this)" id="icon" class="fas fa-folder-open"></i>
                     </p>
                             
                           </td>
