2df2d8d81efc990b2d0de6bc6c81da2a00c52aa0
keshridivya <dkeshari094@gmail.com>
2022-04-25 14:40:51 +0530

    r5 c

--- 1/views/report/R5.php
+++ 1/views/report/R5.php
@@ -427,6 +427,19 @@ if (mysqli_num_rows($complaints)>0){
                   </thead>
                    <tbody>
                    <?php 
+                   /*pagination*/
+                   if (!isset ($_GET['page']) ) {  
+                    $page = 1;  
+                } else {  
+                    $page = $_GET['page'];  
+                }  
+                $results_per_page = 10;  
+                $page_first_result = ($page-1) * $results_per_page;  
+                $query = "select * from criminal";  
+                $result = mysqli_query($conn, $query);  
+                $number_of_result = mysqli_num_rows($result);  
+                $number_of_page = ceil ($number_of_result / $results_per_page);  
+                   /*pagination*/
                    $dat=$_POST['month'];
                    $year=$_POST['year'];
                    $district=$_POST['district'];
@@ -434,7 +447,7 @@ if (mysqli_num_rows($complaints)>0){
                    complaint_form.complaint_filer_address as comp_add,complaint_form.police_station as station,
                    complaint_form.district as district,criminal.criminal_name as crname,criminal.criminal_address as cradd,
                    criminal.age as crage,victim.name as vname,victim.address as vaddress,victim.caste as vcaste,victim.victim_age as vage,victim.caste_certificate as vcertificate,victim.aadhar_card as vaadhar,victim.charge_sheet as vcsheet,image.discription as discrip,
-                  complaint_form.discription as discription from criminal left join complaint_form on complaint_form.id=criminal.caseid left join victim on victim.caseid=complaint_form.id  left join image on image.caseid=complaint_form.id where monthname(date) ='$dat' and year(date)='$year' and district='$district';");
+                  complaint_form.discription as discription from criminal left join complaint_form on complaint_form.id=criminal.caseid left join victim on victim.caseid=complaint_form.id  left join image on image.caseid=complaint_form.id where monthname(date) ='$dat' and year(date)='$year' and district='$district' LIMIT . $page_first_result . ',' . $results_per_page;");
                     $count=1;
                     $num=mysqli_num_rows($sql);
                     if($num==0){
@@ -470,7 +483,8 @@ if (mysqli_num_rows($complaints)>0){
                    <?php $count++; } ?>
                    
 
-
+<tr><td><?php for($page = 1; $page<= $number_of_page; $page++) {  
+    echo '<a href = "index2.php?page=' . $page . '">' . $page . ' </a>';?>  </td></tr>
                   </tbody>
                 
                  </table>
