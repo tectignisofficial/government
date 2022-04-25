cb1b7131a69794319e89e0be6fc1cb2a8d3acda2
keshridivya <dkeshari094@gmail.com>
2022-04-25 16:51:57 +0530

    c

--- 1/views/report/complain.php
+++ 1/views/report/complain.php
@@ -419,16 +419,16 @@ if (mysqli_num_rows($complaints)>0){
                         $stage2=$arr['stage_2'];
                         $stage3=$arr['stage_3'];
                       }
-                      if($stage1==''){
+                      if($stage1=='Null'){
                         echo "Case Open";
                       }
-                      else if($stage1!=''){
+                      else if($stage1!='Null'){
                         echo "First Payment Done";
                       }
-                      else if($stage2!=''){
+                      else if($stage2!='Null'){
                         echo "Second Payment Done";
                       }
-                      else if($stage3!=''){
+                      else if($stage3!='Null'){
                         echo "Case Close";
                       }
                       ?>
