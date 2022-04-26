cbe872c5c0daf956bf404aba6ad896fc0f0d029b
keshridivya <dkeshari094@gmail.com>
2022-04-26 13:11:41 +0530

    C

--- 1/views/report/payment.php
+++ 1/views/report/payment.php
@@ -446,7 +446,7 @@ if($btn4){
                         data:"name="+name+"btn1="+sub1,
                         success: function(data){
                             $('#sec2').fadeIn().css("display","block");
-                            $('#id1').html(data);
+                            //$('#id1').html(data);
                         }
                         
                     });
