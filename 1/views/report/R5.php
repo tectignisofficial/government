524450a6702d8a5bd8ce9ea2daca1a0b38ed54c9
keshridivya <dkeshari094@gmail.com>
2022-04-26 12:46:44 +0530

    c

--- 1/views/report/R5.php
+++ 1/views/report/R5.php
@@ -389,7 +389,7 @@ if (mysqli_num_rows($complaints)>0){
               </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-10" >
-              <table id="tbl mytableID" class="table table-hover sampleTable table-bordered"  >
+              <table id="tbl data" class="table table-hover sampleTable table-bordered"  >
                 <thead>
                   <tr>
                     <th class="text-center"rowspan="2">अ. क्र.</th>
@@ -528,14 +528,28 @@ if (mysqli_num_rows($complaints)>0){
     <!-- /.content -->
   </div>
   <script >
-              $(document).ready(function() {
-		$(".sampleTable").fancyTable({
-			sortColumn:0,
-			pagination: true,
-			perPage:10,
-			globalSearch:true
-		});		
-	});
+            $(document).ready (function () {  
+    $('#data').after ('<div id="nav"></div>');  
+    var rowsShown = 5;  
+    var rowsTotal = $('#data tbody tr').length;  
+    var numPages = rowsTotal/rowsShown;  
+    for (i = 0;i < numPages;i++) {  
+        var pageNum = i + 1;  
+        $('#nav').append ('<a href="#" rel="'+i+'">'+pageNum+'</a> ');  
+    }  
+    $('#data tbody tr').hide();  
+    $('#data tbody tr').slice (0, rowsShown).show();  
+    $('#nav a:first').addClass('active');  
+    $('#nav a').bind('click', function() {  
+    $('#nav a').removeClass('active');  
+   $(this).addClass('active');  
+        var currPage = $(this).attr('rel');  
+        var startItem = currPage * rowsShown;  
+        var endItem = startItem + rowsShown;  
+        $('#data tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).  
+        css('display','table-row').animate({opacity:1}, 300);  
+    });  
+});  
           </script>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
