b004ec7c33cba09b261e226f36567da6d5dd044b
keshridivya <dkeshari094@gmail.com>
2022-04-26 17:22:32 +0530

    c

--- 1/views/report/payment.php
+++ 1/views/report/payment.php
@@ -33,6 +33,7 @@ if(isset($_POST['sub3'])){
     $stage2=$_POST['stage2'];
     $sql=mysqli_query($conn,"UPDATE `bank_detail` SET `stage_2`='$stage2' WHERE caseid='$id1'");
     echo "<script>alert('$id1');</script>";
+    echo "<script>alert('$stage2');</script>";
 }
 
 if(isset($_POST['subt3'])){
