c83f4797920a63397afa699b176f4101a3570802
keshridivya <dkeshari094@gmail.com>
2022-04-28 16:24:14 +0530

    c

--- super_admin/views/report/Complaint.php
+++ super_admin/views/report/Complaint.php
@@ -433,7 +433,7 @@ if (mysqli_num_rows($complaints)>0){
                    <?php 
                    $dat=$_POST['month'];
                    $year=$_POST['year'];
-                   $sql=mysqli_query($conn,"select * from complaint_form where monthname(date) ='$dat' and year(date)='$year' and district='navi mumbai';");
+                   $sql=mysqli_query($conn,"select complaint_form.id as id,complaint_form.book_no as book_no, complaint_form.date as date, complaint_form.district as district, complaint_form.police_station as police_station, complaint_form.complaint_no as complaint_no, complaint_form.section as section, complaint_form.type_of_offence as type_of_offence, bank_detail.stage_1 as stage1, bank_detail.stage_2 as stage2, bank_detail.stage_3 as stage3 from complaint_form inner join bank_detail on complaint_form.id=bank_detail.caseid where monthname(date) ='$dat' and year(date)='$year' and district='navi mumbai';");
                     $count=1;
                     $num=mysqli_num_rows($sql);
                     if($num==0){
@@ -454,7 +454,25 @@ if (mysqli_num_rows($complaints)>0){
                     <td><a href="../editform1.php?action=edit&id=<?php echo $arr["id"]; ?>" title="" class="btn btn-success btn-xs delete_purchase">
 		                          <i class="fas fa-edit"></i>
 		                        </a></td>
-                    
+                            <td id="closeCase">
+                      <?php
+                      $stage1=$arr["stage1"];;
+                      $stage2=$arr["stage2"];;
+                      $stage3=$arr["stage3"];;
+                      if($stage1==''){
+                        echo "Case Open";
+                      }
+                      else if($stage1!='' && $stage2=='' && $stage3==''){
+                        echo "First Payment Done";
+                      }
+                      else if($stage1!='' && $stage2!='' && $stage3==''){
+                        echo "Second Payment Done";
+                      }
+                      else if($stage1!='' && $stage2!='' && $stage3!=''){
+                        echo "Case Close";
+                      }
+                      ?>
+                    </td>
                    </tr>
                    <?php $count++; } ?>
 
@@ -464,7 +482,7 @@ if (mysqli_num_rows($complaints)>0){
                     </tr>
                    <?php 
                    $dat=$_POST['month'];
-                   $query=mysqli_query($conn,"select * from complaint_form where monthname(date) ='$dat' and year(date)='$year' and district='raigarh';");
+                   $query=mysqli_query($conn,"select complaint_form.id as id,complaint_form.book_no as book_no, complaint_form.date as date, complaint_form.district as district, complaint_form.police_station as police_station, complaint_form.complaint_no as complaint_no, complaint_form.section as section, complaint_form.type_of_offence as type_of_offence, bank_detail.stage_1 as stage1, bank_detail.stage_2 as stage2, bank_detail.stage_3 as stage3 from complaint_form inner join bank_detail on complaint_form.id=bank_detail.caseid where monthname(date) ='$dat' and year(date)='$year' and district='raigarh';");
                     $count1=1;
                     $num=mysqli_num_rows( $query);
                     if($num==0){
@@ -485,7 +503,25 @@ if (mysqli_num_rows($complaints)>0){
                     <td><a href="../editform1.php?action=edit&id=<?php echo $arr["id"]; ?>" title="" class="btn btn-success btn-xs delete_purchase">
 		                          <i class="fas fa-edit"></i>
 		                        </a></td>
-                    
+                            <td id="closeCase">
+                      <?php
+                      $stage1=$arr["stage1"];;
+                      $stage2=$arr["stage2"];;
+                      $stage3=$arr["stage3"];;
+                      if($stage1==''){
+                        echo "Case Open";
+                      }
+                      else if($stage1!='' && $stage2=='' && $stage3==''){
+                        echo "First Payment Done";
+                      }
+                      else if($stage1!='' && $stage2!='' && $stage3==''){
+                        echo "Second Payment Done";
+                      }
+                      else if($stage1!='' && $stage2!='' && $stage3!=''){
+                        echo "Case Close";
+                      }
+                      ?>
+                    </td>
                    </tr>
                    <?php $count1++; } ?>
                    
