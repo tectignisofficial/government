3e5f0103680959d564edf7bee165a7bdea0f43f2
keshridivya <dkeshari094@gmail.com>
2022-04-28 15:26:43 +0530

    c

--- 1/views/report/payment.php
+++ 1/views/report/payment.php
@@ -482,20 +482,7 @@ if(isset($_POST['subt3'])){
         })
          
     </script>                     
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-      <strong>Copyright &copy; <?php echo date('Y'); ?></strong>
-      All rights reserved.
-      <div class="float-right d-none d-sm-inline-block">
-        Designd by Tectignis It Solutions</b>
-      </div>
-    </footer>
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
-       <!-- ./wrapper -->
+      
 
     <!-- jQuery -->
     <script src="../../plugins/jquery/jquery.min.js"></script>
