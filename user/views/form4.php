265f2831769a012dd45259c95ba53490d5c2bc6d
keshridivya <dkeshari094@gmail.com>
2022-05-09 12:18:52 +0530

    c

--- user/views/form4.php
+++ user/views/form4.php
@@ -58,6 +58,10 @@ if(isset($_POST['subm_it'])){
     <link rel="shortcut icon" type="image/png" href="../dist/img/government.png" />
 
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/docs-app/css/compiled-4.20.0.min.css">
+
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
