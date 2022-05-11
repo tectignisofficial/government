daafc9cf8fe7e8d788e25c1c46df3c085f571dc5
keshridivya <dkeshari094@gmail.com>
2022-05-11 14:20:59 +0530

    c

--- user/views/editform4.php
+++ user/views/editform4.php
@@ -367,7 +367,7 @@ if(isset($_POST['subm_it']) && ($_GET['eid'])){
                       </div><?php if($row['course'] == 'BSIT' ) { echo "selected='selected'"; }?>
                       <select class="custom-select" id="gender3" name="payStatus">
                         <option value="<?php  $arr['status']; ?>"><?php echo $arr['status']; ?></option>
-                        <option value="stage 1" selected>stage 1 payment</option>
+                        <option value="stage 1" >stage 1 payment</option>
                         <option value="stage 2">stage 2 payment</option>
                         <option value="stage 3">stage 3 payment</option>
                         <option value="stage 3">Case close</option>
