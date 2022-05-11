73cbae05fc3ee5a247937968eff1cf30bd8334ff
keshridivya <dkeshari094@gmail.com>
2022-05-11 13:17:35 +0530

    c

--- user/views/editform1.php
+++ user/views/editform1.php
@@ -398,16 +398,17 @@ echo mysqli_query($conn);
                           
                        <!-- </select>-->
                       </div>
-                      <div class="form-group col-6">
+                      <div class="form-group col-6" id="raigarh" >
                         <label for="Exampleपोलीस ठाणे" class="form-label">पोलीस ठाणे</label>
-                         <input type="Dropdown" name="police_station  value="<?php echo $police_station?> class="form-control " id="Exampleपोलीस ठाणे" placeholder=""> 
+                       <input type="Dropdown" name="police_station  value="<?php echo $police_station?> class="form-control " id="Exampleपोलीस ठाणे" placeholder=""> 
                         <!--<select id="police" required name="police_station" class="form-control">
                           <option  value="<?php //echo $police_station?>"><?php// echo $police_station?></option>
                         
                         </select>-->
                       </div>
 
-                     
+                      
+                    </div>
                       
 
                    
