122083ce69b143578e3c18290bde0f431a663a67
keshridivya <dkeshari094@gmail.com>
2022-05-13 17:21:50 +0530

    c

--- user/views/index.php
+++ user/views/index.php
@@ -295,8 +295,6 @@
                   if(isset($_POST['datepicker'])){
                     $date = $_POST['datepicker'];
                   $query=mysqli_query($conn,"select * from complaint_form where  district='$district' and year(date)='$date' and type_of_offence='शिवीगाळ,'");
-                }else{
-                  $query=mysqli_query($conn,"select * from complaint_form where  district='$district' and year(date)='2022' and type_of_offence='शिवीगाळ,'");
                 }
                 $count1=mysqli_num_rows($query);
                   ?>
