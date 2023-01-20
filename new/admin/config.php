9f45b2edbb4343bdf8dfc19a60d5e51234a52026
Namrata <nvjadhav22@gmail.com>
2023-01-20 13:31:14 +0530

    database

--- new/admin/config.php
+++ new/admin/config.php
@@ -1,4 +1,4 @@
 <?php
 
-   $con = new mysqli("151.106.124.51","u188140722_z1new","Admin@123","u188140722_z1new");
+   $con = new mysqli("151.106.124.51","u188140722_atrocity","@trocity#Admin2023","u188140722_atrocity");
 ?>
